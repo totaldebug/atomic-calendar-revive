d2c52b22a5e8ec0c02f6e01e4a9ef930c31da347
Wojciech Mamak <atomic7777@gmail.com>
2019-02-10 12:03:05 +0100

    Simple calendar with working event titles

--- atomic_calendar.js
+++ atomic_calendar.js
@@ -14,54 +14,149 @@ class AtomicCalendar extends LitElement {
     return {
       hass: Object,
       config: Object,
+	  content: Object
     }
   }
 
 	constructor() {
 		super();
 		this.lastUpdateTime;
+		this.events;
+		this.content=html`<div>HTML before calendar read</div>`;
+		this.shouldUpdateHtml = false;
 
 		
 	}
+  
+  updated(){
+   
+  }  
   
 _render({ hass, config }) {
 
-if(!this.isUpdating)
-	(async () => {
-		this.isUpdating=true;
-		// wait for moment.js or continue if already loaded
-		if(typeof(moment) == "undefined")
-			await this.loadScript('https://unpkg.com/moment@2.23.0/moment.js').then(() => {
-		});
+	if(!this.isUpdating){
+		(async () => {
+			this.isUpdating=true;
 
-		
-		// get events from HA Calendar each 15 minutes
-		if (!this.lastUpdateTime || moment().diff(this.lastUpdateTime,'minutes') > 15) {
-			await this.getEvents();
-			this.lastUpdateTime = moment();
+			// wait for moment.js or continue if already loaded
+			if(typeof(moment) == "undefined"){
+				//await this.loadScript('https://unpkg.com/moment@2.23.0/moment.js')
+				await this.loadScript('/local/moment-with-locales.min.js')
+				moment.locale(this.hass.language);
+				const events = await this.getEvents()
+				this.updateHTML(events);
+
 			}
-	//console.log('end of update');
-	this.isUpdating=false;
+
+			
+
+			// get events from HA Calendar each 15 minutes
+			else if (!this.lastUpdateTime || moment().diff(this.lastUpdateTime,'minutes') > 15) {
+				 // this.getEvents();
+				moment.locale(this.hass.language);
+				const events = await this.getEvents()
+				this.lastUpdateTime = moment();
+				this.shouldUpdateHtml = true;
+				this.updateHTML(events);	
+				}
+
+		this.isUpdating=false;
+		}
+		)()
 	}
-	)()
+	return this.content;
 
-    return html`
-      <style>
-  
-      </style>
-	  
-
-	  <ha-card>
-		<div>
-			${this.config.title}
-		</div>
-		
-		
-	  </ha-card>
-
-     
-    `;
   }
+
+
+  setStyle(){
+	return html`
+		<style>
+			.cal-card{
+				display: flex;
+			}
+			.cal-title {
+				font-size: var(--paper-font-headline_-_font-size);
+				color: var(--primary-text-color);
+				padding: 16px 16px 5px 16px;
+				
+			}
+			table{
+				color:black;
+				border: 1px solid #ccc;
+				border-spacing: 0;
+				display: flex;
+				flex-flow: column nowrap;
+				flex: 1 1 auto;
+			}
+
+			td {
+				display: flex;
+				flex-flow: row nowrap;
+				}
+			
+			tr{
+				width: 100%;
+				display: flex;
+				flex-flow: row nowrap;
+			}
+
+			.event-row {
+				
+				flex-direction: row;
+			}
+
+			.event-date {
+				
+        
+				flex: 0 0 20px;
+				padding: 5px;
+				border: 1px solid #ccc;
+				color: blue;
+			}
+			
+			.event-main {
+				display:flex;
+				flex: 1 1 auto;
+				flex-direction:column;
+				padding: 5px;
+				border: 1px solid #ccc;
+				color: red;
+			}
+			
+			.event-location {
+		
+
+				flex: 0 0 40px;
+				padding: 5px;
+				border: 1px solid #ccc;
+				color: red;
+				
+			.event-title {
+				flex: 1 1 auto;
+				padding: 5px;
+				border: 1px solid #ccc;
+				color: red;
+			}		
+			
+			.event-time {
+				flex: 1 1 auto;
+				padding: 5px;
+				border: 1px solid #ccc;
+				color: red;
+			}			
+				
+		</style>
+
+		`
+
+	}
+
+  updateEvents() {
+			this.lastUpdateTime = moment();
+			this.shouldUpdateHtml = true;
+	}
+
 
   setConfig(config) {
     if (!config.entities) {
@@ -95,41 +190,116 @@ if(!this.isUpdating)
     });
   }
 
+  
+  eventHTML(event) {
+	
+	return html`<div>${event.startTime.format('DD')}</div>`
+   }
+
+
+hoursHTML(event) {
+	return html`
+		
+	`
+}
+
+  updateHTML(events){
+	
+	// check if no events 
+	// TODO: write something if no events
+	if (events.length==0)	
+		{
+			return 'No events in next days'
+		}
+	
+	const table = ``
+		
+
+	// grouping events by days
+	const groupsOfEvents = events.reduce(function (r,a) {
+			r[a.daysToSort] = r[a.daysToSort] || []
+			r[a.daysToSort].push(a);
+			return r
+		}, Object.create(null))
+
+	
+	//loop through days
+	const htmlDays=Object.values(groupsOfEvents).map((d) => {
+		
+		//loop through events for each day
+		const htmlEvents=Object.values(d).map((event,i) => {
+	
+				const hours = this.hoursHTML(event)
+	
+				return html`
+				<tr>
+					<td class="event-date"><div>
+							<div>${i===0 ? event.startTime.format('dd') : ''}</div>
+							<div>${i===0 ? event.startTime.format('DD') : ''}</div>
+							<div></div>
+						</div></td>
+					<td class="event-main">
+							<div class="event-title">${event.title}</div>
+							<div class="event-time">Date</div>
+					</td>
+					<td class="event-location">Location</td>
+				</tr>`
+			})
+		
+		//daily html
+		return htmlEvents
+	})
+
+		
+this.content =  html`
+      ${this.setStyle()}
+	  
+
+	  <ha-card>
+		<div class="cal-title" >
+			${this.config.title}
+		</div>
+		<div>
+			<table><tbody>
+				${htmlDays}
+			</tbody></table>
+		</div>
+	  </ha-card>
+
+     
+    `;
+
+	}
+
 
    /**
    * gets events from HA Calendar
    * 
    */
-	getEvents() {
+	async getEvents() {
 		let start = moment().startOf('day').format('YYYY-MM-DDTHH:mm:ss');
 		let end = moment().add(this.config.maxDaysToShow-1, 'days').format('YYYY-MM-DDTHH:mm:ss');
 		let calendarUrlList = this.config.entities.map(entity => 
 		`calendars/${entity.entity}?start=${start}Z&end=${end}Z`)
 		//getting data from HA
-		let calendarEvents = Promise.all(calendarUrlList.map(url => this.hass.callApi('get',url)))
-		.then((haEvent) => { 
-			let events = [].concat.apply([], (haEvent.map((singleCalEvents,i) => {
+
+		return await (Promise.all( calendarUrlList.map(url => 
+		this.hass.callApi('get',url))).then((result) => {
+				let ev = [].concat.apply([], (result.map((singleCalEvents,i) => {
 				//getting each event from each calendar, passing settings, assuming calendars were resolved in the correct order
 				return singleCalEvents.map(evt =>  new SingleEvent(evt,this.config.entities[i]))
 			})))
-		console.log(events)
-
-			events.forEach(element =>
-			{console.log(element.startTime)
-				console.log(element.isFullDayEvent)
-			})
-	
-		})
-		.catch((error) => { console.log(error) });
-
-
+		// sort events
+		ev = ev.sort((a,b) => moment(a.startTime) - moment(b.startTime)   )
+		return ev}))
 	}
 
+	
    /**
    * loads moment.pl
    * @return {Promise}
    */
-	loadScript(src) {
+	async loadScript(src) {
 		return new Promise(resolve => {
 			const script = document.createElement('script');
 			script.src = src;
@@ -164,12 +334,12 @@ class SingleEvent {
 	
 	get startTime() {
 		if (this.singleEvent.start.dateTime) return moment(this.singleEvent.start.dateTime)
-		else return moment(this.singleEvent.start.date)
+		else return moment(this.singleEvent.start.date).startOf('day')
 	}
 	
 	get endTime() {
 		if (this.singleEvent.end.dateTime) return moment(this.singleEvent.end.dateTime)
-		else return moment(this.singleEvent.start.date)
+		else return moment(this.singleEvent.start.date).endOf('day')
 	}
 	
 	get isFullDayEvent() {
@@ -177,5 +347,21 @@ class SingleEvent {
 			return true
 		else return false
 	}
+	
+
+	// return YYYYMMDD for sorting
+	get daysToSort() {
+		return moment(this.singleEvent.start.dateTime || this.singleEvent.start.date).format('YYYYMMDD');
+	}
+	get isEventToday(){
+			return (moment(this.singleEvent.start.dateTime || this.singleEvent.start.date).isSame(moment(), 'day') ?  true :  false);
+	}
+	
+	get isEventTomorrow() {
+			return moment(this.singleEvent.start.dateTime || this.singleEvent.start.date).isSame(moment().add(1,'day'), 'day') ?  true :  false;
+
+	}
+
+
 	
 }
