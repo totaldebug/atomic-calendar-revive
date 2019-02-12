d1f9e176d1b0adfa373e90313263185ea6b10372
Wojciech Mamak <atomic7777@gmail.com>
2019-02-12 16:37:40 +0100

    Most of options working, showing correctly

--- atomic_calendar.js
+++ atomic_calendar.js
@@ -22,7 +22,6 @@ class AtomicCalendar extends LitElement {
 		this.content=html`<div>HTML before calendar read</div>`;
 		this.shouldUpdateHtml = false;
 
-		
 	}
   
   updated(){
@@ -70,7 +69,8 @@ _render({ hass, config }) {
 	return html`
 		<style>
 			.cal-card{
-				display: flex;
+		
+				padding: 8px;
 			}
 			.cal-title {
 				font-size: var(--paper-font-headline_-_font-size);
@@ -80,80 +80,105 @@ _render({ hass, config }) {
 			}
 			table{
 				color:black;
-				border: 1px solid #ccc;
-				border-spacing: 0;
-				display: flex;
+			/*	border: 1px solid #ccc;*/
+			/*	border-spacing: 0;*/
+				/*display: flex;
 				flex-flow: column nowrap;
-				flex: 1 1 auto;
+				flex: 1 1 auto;*/
 				margin-left: 5px;
 				margin-right: 5px;
+				
+				
+				  border-collapse: separate;
+  border-spacing: 10px 5px;
+   border-collapse: collapse;
+				
 			}
 
 			td {
 				/*display: flex;
 				flex-flow: row nowrap;*/
+				
+						
 				}
 			
 			tr{
 				width: 100%;
 				/*display: flex;
 				flex-flow: row nowrap;*/
+
+				
+								 
 			}
 
-			.event-row {
-				
-				flex-direction: row;
-			}
+		
 
-			.event-date {
+			.event-left {
 				
-				flex: 0 0 20px;
+			/*	flex: 0 0 20px;*/
 				padding: 5px;
-				
+				padding-top: 7px;
+				padding-left: 8px;
+				padding-right: 10px;
 				text-align: center;
-				border: 1px solid #ccc;
+		
 				color: ${this.config.dateColor};
 				font-size: ${this.config.dateSize}%;
 				vertical-align: top;
+						 
+			}
+			
+			.event-right {
+				/* display:inline-block;*/
+				 display: flex;
+				 justify-content: space-between;
+				padding: 5px;
+						 
 			}
 			
 			.event-main {
-			/*	display:flex;*/
-				flex: 1 1 auto;
-				flex-direction:column;
-				padding: 5px;
-				border: 1px solid #ccc;
-				
+				/*display:flex;*/
+				/*flex: 1 1 auto;
+				flex-direction:row nowrap;*/
+
+				/*border: 1px solid #ccc;*/
+				/*flex-direction: row */
+				  display: inline-block;
+				  vertical-align: top;
+		
 			}
 			
 			.event-location {
-		
-				padding: 5px;
-				flex: 0 0 40px;
-			
-				border: 1px solid #ccc;
-				color: red;
 				text-align: right;
-				
-}
+				display: inline-block;
+				vertical-align: top;
+			}
 				
 			.event-title {
-				flex: 1 1 auto;
+				/*flex: 1 1 auto;*/
 				font-size: ${this.config.titleSize}%;
 				color: ${this.config.titleColor};
 				
 			}		
 			
 			.event-time {
-				flex: 1 1 auto;
+				/*flex: 1 1 auto;*/
 			
 				font-size: ${this.config.timeSize}%;
 				color: ${this.config.timeColor};
 			}			
 			
 			.event-location-icon {
-			    height: 14px;
-                width: 14px;
+			    height: 15px;
+                width: 15px;
+				color: ${this.config.locationIconColor};
+				margin-top: -2px;
+			}
+
+			.location-link {
+				color: ${this.config.locationLinkColor};
+				text-decoration: none;
+				font-size: ${this.config.locationTextSize}%;
 			}
 				
 		</style>
@@ -173,18 +198,29 @@ _render({ hass, config }) {
       throw new Error('You need to define entities');
     }
     this.config = {
+		// text translations
 		title: 'Kalendarz',
 		fullDayEventText: 'Cały dzień',
+		untilText: 'Do', 
+		
+		// main settings
 		showColors: true,  // show calendar title colors, if set
 		maxDaysToShow: 7,
+		showLocation: true,
+		showMonth: false, // show month under day
+
 		dateColor: 'var(--primary-text-color)', // Date text color
 		dateSize: 90, //Date text size
 
 		timeColor: 'var(--primary-color)', // Time text color
 		timeSize: 90, //Time text size
 
-		titleColor: 'var(--primary-text-color)',
+		titleColor: 'var(--primary-text-color)', //Event title settingss
 		titleSize: 100,
+
+		locationIconColor: 'rgb(230, 124, 115)', //Location link settings
+		locationLinkColor: 'var(--primary-text-color)',
+		locationTextSize: 90,
 		...config
 		
 	}
@@ -218,44 +254,37 @@ _render({ hass, config }) {
 
 	// generating Event Title HTML
 	getTitleHTML(event) {
-		console.log(event)
+		//console.log(event)
 		const titleColor = (this.config.showColors && event.config.color!== "undefined") ? event.config.color : this.config.titleColor
-		console.log(titleColor)
+	
 		return html`
-		
-		<div class="event-title" style="color: ${titleColor}">${event.title}</div>
+		<a href="${event.link}" style="text-decoration: none;" target="_blank">
+		<div class="event-title" style="color: ${titleColor}">${event.title}</div></a>
 		`
 	}
 	
 	//generating Event Time HTML
 	getHoursHTML(event) {
-		var hours = ''
 		
+		// 1. Full day event, one day only
 		if (event.isFullDayEvent)
-			return html`
-			<div >${this.config.fullDayEventText}</div>`
-
-		if(event.startTime < moment().startOf('day'))
-			hours += event.startTime.format('LT') + ' - '
-
-
-		hours += event.endTime.format('LT')
-				
-		return html`
-			<div>${hours}</div>
-		`
+			return html`<div >${this.config.fullDayEventText}</div>`
+		// 2. Event ends another day
+		else if (event.endTime > moment().endOf('day'))
+			{
+				return html`
+				<div>${this.config.untilText} ${event.endTime.format('LL')}</div>`
+			}
+		// 3. Normal one day event, with time set
+		else return html`
+			<div>${event.startTime.format('LT')} - ${event.endTime.format('LT')}</div>`
 	}
 
-		getLocationHTML(event) {
-		var location = ''
-		if (!event.location) return html``
-		
-			location+=''
-			location+=event.address
-		
-
-		return html`
-			<div><ha-icon class="event-location-icon" icon="mdi:map-marker"></ha-icon>&nbsp;${location}</div>
+	getLocationHTML(event) {
+	
+		if (!event.location || !this.config.showLocation) return html``
+		else return html`
+			<div><a href="https://maps.google.com/?q=${event.location}" target="_blank" class="location-link"><ha-icon class="event-location-icon" icon="mdi:map-marker"></ha-icon>&nbsp;${event.address}</a></div>
 		`
 	}
 	
@@ -281,25 +310,35 @@ _render({ hass, config }) {
 
 		
 		//loop through days
-		const htmlDays=Object.values(groupsOfEvents).map((d) => {
+		const htmlDays=Object.values(groupsOfEvents).map((d, di) => {
 			
 			//loop through events for each day
 			const htmlEvents=Object.values(d).map((event,i) => {
-		
-					const hours = this.getHoursHTML(event)
+					const dayWrap = (i==0 && di > 0) ? 'border-top: 1px solid ;' : ''
+					//const hours = this.getHoursHTML(event)
 		
 					return html`
-					<tr>
-						<td class="event-date"><div>
-								<div>${i===0 ? event.startTime.format('DD') : ''}</div>
-								<div>${i===0 ? event.startTime.format('ddd') : ''}</div>
+			<tr style="${dayWrap} ">
+						<td class="event-left"><div>
+								<div>${(i===0 && this.config.showMonth) ? event.startTimeToShow.format('MMM') : ''}</div>
+								<div>${i===0 ? event.startTimeToShow.format('DD') : ''}</div>
+								<div>${i===0 ? event.startTimeToShow.format('ddd') : ''}</div>
+
 								<div></div>
-							</div></td>
-						<td class="event-main">
-								${this.getTitleHTML(event)}
-								<div class="event-time">${this.getHoursHTML(event)}</div>
+							</td>
+						<td >
+							<div class="event-right">
+								<div class="event-main">
+									${this.getTitleHTML(event)}
+									<div class="event-time">${this.getHoursHTML(event)}</div>
+								</div>
+								<div class="event-location">
+									${this.getLocationHTML(event)}
+								</div>
+							</div>
+							<div></div>
 						</td>
-						<td class="event-location">${this.getLocationHTML(event)}</td>
+						
 					</tr>`
 				})
 			
@@ -312,7 +351,7 @@ this.content =  html`
       ${this.setStyle()}
 	  
 
-	  <ha-card>
+	  <ha-card class="cal-card">
 		<div class="cal-title" >
 			${this.config.title}
 		</div>
@@ -347,7 +386,7 @@ this.content =  html`
 				return singleCalEvents.map(evt =>  new SingleEvent(evt,this.config.entities[i]))
 			})))
 		// sort events
-		ev = ev.sort((a,b) => moment(a.startTime) - moment(b.startTime)   )
+		ev = ev.sort((a,b) => moment(a.startTimeToShow) - moment(b.startTimeToShow)   )
 		return ev}))
 	}
 
@@ -389,26 +428,45 @@ class SingleEvent {
 		return this.singleEvent.summary
 	}
 	
+	//true start time
 	get startTime() {
 		if (this.singleEvent.start.dateTime) return moment(this.singleEvent.start.dateTime)
-		else return moment(this.singleEvent.start.date).startOf('day')
+		else return moment(this.singleEvent.start.date).local().startOf('day')
+	}
+
+	//start time, but returns today if before today
+	get startTimeToShow() {
+		var time = this.singleEvent.start.dateTime ? moment(this.singleEvent.start.dateTime) : moment(this.singleEvent.start.date).local().startOf('day')
+		if (moment(time) < moment().startOf('day'))
+			return moment().startOf('day') 
+		    else return time
 	}
 	
 	get endTime() {
 		if (this.singleEvent.end.dateTime) return moment(this.singleEvent.end.dateTime)
-		else return moment(this.singleEvent.start.date).endOf('day')
+		
+		else return moment(this.singleEvent.end.date).subtract(1, 'days').endOf('day')
 	}
 	
+	// is full day event, but only one day
 	get isFullDayEvent() {
-		if (!this.singleEvent.start.dateTime && !this.singleEvent.end.dateTime)
+	console.log	(this.singleEvent.start.date)
+		console.log	(this.singleEvent.end.date)
+		if (!this.singleEvent.start.dateTime && !this.singleEvent.end.dateTime && moment(this.singleEvent.start.date).isSame(moment(this.singleEvent.end.date).subtract(1, 'days'), 'day'))
+				return true
+		else return false
+	}
+	
+	// is full day event, more days
+	get isFullMoreDaysEvent() {
+		if (!this.singleEvent.start.dateTime && !this.singleEvent.end.dateTime && this.singleEvent.start.date !== this.singleEvent.end.date)
 			return true
 		else return false
 	}
 	
-
 	// return YYYYMMDD for sorting
 	get daysToSort() {
-		return moment(this.singleEvent.start.dateTime || this.singleEvent.start.date).format('YYYYMMDD');
+		return moment(this.startTimeToShow).format('YYYYMMDD');
 	}
 	get isEventToday(){
 			return (moment(this.singleEvent.start.dateTime || this.singleEvent.start.date).isSame(moment(), 'day') ?  true :  false);
@@ -420,11 +478,15 @@ class SingleEvent {
 	}
 
 	get location() {
-		return this.singleEvent.location;
+		return this.singleEvent.location ? (this.singleEvent.location).split(' ').join('+') : '';
 
 	}
 	
 	get address() {
 		return this.singleEvent.location ? this.singleEvent.location.split(',')[0] : ''
+	}
+
+	get link() {
+		return this.singleEvent.htmlLink
 	}
 }
