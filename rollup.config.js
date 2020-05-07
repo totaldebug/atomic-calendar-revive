65929ec681ef097af8ab18bc224dd2de3cefdaf7
Steven Marks <marksie1988@users.noreply.github.com>
2020-05-07 15:47:56 +0000

    Change Rollup & Dev Container

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,37 @@
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 import commonjs from 'rollup-plugin-commonjs';
+import serve from 'rollup-plugin-serve';
+
+const dev = process.env.ROLLUP_WATCH;
+
+const serveopts = {
+    contentBase: ['./dist'],
+    host: '0.0.0.0',
+    port: 5000,
+    allowCrossOrigin: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+};
+
+const plugins = [
+    resolve(),
+    commonjs(),
+    dev && serve(serveopts),
+    !dev && terser(),
+];
+
 
 export default {
     input: ['./src/app.js', './src/app-editor.js'],
     output: {
         dir: 'dist',
         entryFileNames: '[name].js',
-        chunkFileNames: '[name].js' 
+        chunkFileNames: '[name].js'
     },
 	watch: {
     clearScreen: false
     },
-    plugins: [
-        resolve(),
-        commonjs(),
-        terser()
-    ]
+    plugins: [...plugins],
 };
