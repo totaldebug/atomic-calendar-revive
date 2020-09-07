2c81414b5664e42d67012b5e187656fe31eba78d
Steven Marks <marksie1988@users.noreply.github.com>
2020-09-07 11:00:40 +0000

    :bug: Fix issue with progress bar

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 import serve from 'rollup-plugin-serve';
-import json from '@rollup/plugin-json'
+import json from '@rollup/plugin-json';
 
 const dev = process.env.ROLLUP_WATCH;
 
@@ -25,6 +25,7 @@ const plugins = [
     json(),
     babel({
         exclude: 'node_modules/**',
+        babelHelpers: 'bundled'
     }),
     dev && serve(serveopts),
     !dev && terser(),
