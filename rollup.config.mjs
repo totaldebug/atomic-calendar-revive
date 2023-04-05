cd492bb9445665ce05398aea53c631e0c4d823df
marksie1988 <marksie1988@users.noreply.github.com>
2023-04-05 11:05:18 +0000

    fix: upgrade deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import json from '@rollup/plugin-json';
 import eslint from '@rollup/plugin-eslint';
 
